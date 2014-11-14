b85509c156d41af84682bc0a6e8d0af64049a89b
Galih Pranowo <gapraart@gmail.com>
2014-11-14 15:17:33 +0000

    Modified controller and helper

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -2,32 +2,28 @@ require 'test_helper'
 
 class StaticPageControllerTest < ActionController::TestCase
   
-  def setup
-    @base_title = "Ruby on Rails web application"
-  end
-  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails web application"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails web application"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Ruby on Rails web application"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "Contact | Ruby on Rails web application"
   end
 
 end
